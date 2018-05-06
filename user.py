947f058301042681b7c9c907cd7c79fad63a1593
claudia njeri <claudianjeri04@gmail.com>
2018-05-06 19:43:49 +0300

    added log_in test and method..this took a while, damn!

--- user.py
+++ user.py
@@ -1,7 +1,8 @@
 class User:
-    user_createaccount= []
+    
+    user_createaccount = []
 
-    def __init__(self, first_name, last_name, email, password):
+    def __init__(self,first_name,last_name,email,password):
 
         self.first_name = first_name
         self.last_name = last_name
@@ -11,4 +12,12 @@ class User:
     def save_account(self):
         User.user_createaccount.append(self)
 
-    
+    @classmethod
+    def account_exists(cls, email, password):
+
+        for account in cls.user_createaccount:
+            if account.email == email and account.password == password:
+
+                return True
+        
+        return False  
--- user_test.py
+++ user_test.py
@@ -10,22 +10,33 @@ class TestUser(unittest.TestCase):
     '''
 
     def setUp(self):#Setup Method is for defining instructions that will be executed before each test method.
-        self.create_account = User("Claudia", "Njeri", "claudianjeri04@gmail", "claudia")
+        self.new_account = User("Claudia", "Njeri", "claudianjeri04@gmail.com", "claudia")
 
-    # def tearDown(self):
-    #     User.user_createaccount = []
+    def tearDown(self):#This code is executed before each test case.
+                       #and its work is to clean up after each test case has run.
+        User.user_createaccount = []
 
     def test_init(self):
-        self.assertEqual(self.create_account.first_name, "Claudia")
-        self.assertEqual(self.create_account.last_name, "Njeri")
-        self.assertEqual(self.create_account.email, "claudianjeri04@gmail.com")
-        self.assertEqual(self.create_account.first_name, "03134323")
+        self.assertEqual(self.new_account.first_name, "Claudia")
+        self.assertEqual(self.new_account.last_name, "Njeri")
+        self.assertEqual(self.new_account.email, "claudianjeri04@gmail.com")
+        self.assertEqual(self.new_account.password, "claudia")
 
-    def test_first_name(self):
-        self.create_account.save_account()
+    def test_save_account(self):
+        self.new_account.save_account()
         self.assertEqual(len(User.user_createaccount), 1)
 
-    
+    # def test_first_name(self):
+    #     self.new_account.save_account()
+    #     self.assertEqual(len(User.user_createaccount),2)
+
+    def test_log_in(self):
+        self.new_account.save_account()
+        log_in = User("Claudia", "Njeri", "claudianjeri04@gmail.com", "claudia")
+        log_in.save_account()
+
+        account_exists = User.account_exists("claudianjeri04@gmail.com", "claudia")
+        self.assertTrue(account_exists)
 
 if __name__ == '__main__':
     unittest.main()
